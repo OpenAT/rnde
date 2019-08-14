9dcb7dde49980082274358d8722679fb3034acc5
Michael Karrer <michaelkarrer81@gmail.com>
2019-08-14 15:56:50 +0200

    [ADD] rnde_contact_form

---
+++ addons/rnde_contact_form/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'rnde_contact_form',
+    'summary': '''FS-Online rnde contact form fields''',
+    'description': '''
+FS-Online rnde contact form fields
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fso_forms',
+    ],
+    'data': [
+
+    ],
+}
---
+++ addons/rnde_contact_form/crm_lead.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-'8' "-*-"
+from openerp import models, fields
+
+
+class CrmLeadRNDE(models.Model):
+    _name = "crm.lead"
+    _inherit = "crm.lead"
+
+    rnde_anrede = fields.Char('Anrede')
+    rnde_titel = fields.Char('Titel')
+
+    rnde_vorname = fields.Char('Vorname')
+    rnde_nachname = fields.Char('Nachname')
+
+    rnde_firmenname = fields.Char('Firmenname')
+
+    rnde_clownpartner_nr = fields.Char('Clownpartner Nummer')
+
+    rnde_nachricht = fields.Text('Nachricht')
+
+    rnde_newsletter = fields.Boolean('Newsletter')
+
+

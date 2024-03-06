f8cce7037aee08c39cb090cac78ce3ce2e69cb5e
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2024-03-06 10:53:48 +0100

    [CHG] enable dynamic price suggestions depending on interval

--- addons/rnde_config/static/src/js/shopFunctions.js
+++ addons/rnde_config/static/src/js/shopFunctions.js
@@ -6,6 +6,7 @@ $(document).ready( function() {
 
     setupPriceDonate();
     setupRemoveZeroDecimals();
+    setupAutoRecomputePriceSuggestions();
 });
 
 function setupPriceDonate() {
@@ -50,6 +51,31 @@ function setupPriceDonate() {
 
         if (priceDonate && !suggestionMatched) {
              document.getElementsByClassName('price_donate').item(0).classList.add("active");
+        }
+    }
+}
+
+function setupAutoRecomputePriceSuggestions() {
+    const header = document.getElementById('payment_intervals');
+
+    if (!header || header.getAttribute('data-auto-recompute-price-donate') !== 'True')
+        return;
+
+    updatePriceSuggestion();
+
+    const interval_select = document.getElementsByName('payment_interval_id');
+
+    if (interval_select.length == 1) {
+        interval_select.item(0).addEventListener("change", e => {
+            updatePriceSuggestion();
+        });
+    } else {
+        const elements = document.querySelectorAll('[data-payment-interval-length-in-months]');
+
+        for (let i = 0; i < elements.length; i++) {
+            elements.item(i).addEventListener("change", e => {
+                updatePriceSuggestion();
+            });
         }
     }
 }
@@ -122,5 +148,58 @@ function setupRemoveZeroDecimals() {
                 .replace(',', '')
                 .replace('.', '');
         }
+    }
+}
+
+function setupAutoRecomputePriceSuggestions() {
+    const header = document.getElementById('payment_intervals');
+
+    if (!header || header.getAttribute('data-auto-recompute-price-donate') !== 'True')
+        return;
+
+    updatePriceSuggestion();
+
+    const interval_select = document.getElementsByName('payment_interval_id');
+
+    if (interval_select.length == 1) {
+        interval_select.item(0).addEventListener("change", e => {
+            updatePriceSuggestion();
+        });
+    } else {
+        const elements = document.querySelectorAll('[data-payment-interval-length-in-months]');
+
+        for (let i = 0; i < elements.length; i++) {
+            elements.item(i).addEventListener("change", e => {
+                updatePriceSuggestion();
+            });
+        }
+    }
+}
+
+function updatePriceSuggestion() {
+    var interval = 1;
+    let selectedInterval = document.querySelectorAll('[data-payment-interval-length-in-months]:checked');
+
+    if (!selectedInterval) {
+        selectedInterval = document.querySelectorAll('[data-payment-interval-length-in-months]:selected');
+    }
+
+    if (selectedInterval) {
+        var interval_months = selectedInterval.item(0).getAttribute('data-payment-interval-length-in-months');
+        if (interval_months == 0) {
+            interval_months = 12;
+        }
+        interval = 12 / interval_months;
+    }
+
+    const elements = document.querySelectorAll('[data-price-original]');
+
+    for (let i = 0; i < elements.length; i++) {
+        const el = elements.item(i);
+        const new_price = el.getAttribute('data-price-original') / interval;
+        const new_price_rounded = Math.round((new_price + Number.EPSILON) * 10) / 10;
+        el.setAttribute('data-price', new_price_rounded);
+        el.value = String(new_price_rounded).replace(',', '').replace('.', ',');
+        $(el).data('price', new_price_rounded);
     }
 }
